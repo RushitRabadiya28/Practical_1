bad4f90243fa655dd5aa3f349828d8b11df08c0e
Rushit_Rabadiya <patelrushit9921@gmail.com>
2022-07-28 22:08:36 +0530

    Del

--- TestCylinder.java
+++
@@ -1,8 +0,0 @@
-public class TestCylinder {
-    public static void main(String[] args) {
-        Cylinder c= new Cylinder();
-        c.getArea();
-        Cylinder c2 = new Cylinder(123 , 123);
-        c2.getArea();
-    }
-}
