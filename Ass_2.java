ee6299129db3330e17a5334bd5b96463ef7e9f59
RushitRabadiya28 <110188571+RushitRabadiya28@users.noreply.github.com>
2022-07-29 20:01:57 +0530

    Add files via upload
    new  code done

---
+++ Ass_2.java
@@ -0,0 +1,14 @@
+public class Ass_2 {
+
+    static void startOZ(String a)
+    {
+        if(a.charAt(0)=='O' || a.charAt(0)=='o')
+        {
+            System.out.print(a.charAt(0));
+        }
+        if(a.charAt(1)=='z' || a.charAt(1)=='Z')
+        {
+            System.out.print(a.charAt(1));
+        }
+    }
+}
---
+++ Ass_2_main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Ass_2_main {
+    public static void main(String[] args) {
+        Scanner ssc=new Scanner(System.in);
+        Ass_2 as = new Ass_2();
+        System.out.print("Enter String:");
+        String A=ssc.nextLine();
+       as.startOZ(A);
+    }
+}
