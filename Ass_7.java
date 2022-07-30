1899113fa3b021a6777ff407cf5139aa4c5770ca
RushitRabadiya28 <110188571+RushitRabadiya28@users.noreply.github.com>
2022-07-30 15:14:41 +0530

    Add files via upload

---
+++ Ass_7.java
@@ -0,0 +1,27 @@
+public class Ass_7 {
+    private static final String string = " ";
+    //public static void main(String[] args)
+    void runFunction()
+    {
+        int number = 0;
+        for (int row = 0; row <= 7; row++) {
+
+            for (int column = 1; column <= 7 - row; column++) {
+                System.out.printf("%4s", string);
+            }
+
+            for (int column = 0; column <= row; column++) {
+                number = (int) Math.pow(2, column);
+
+                System.out.printf("%4d", number);
+            }
+
+            for (int column = row - 1; column >= 0; column--) {
+                number = (int) Math.pow(2, column);
+
+                System.out.printf("%4d", number);
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ Ass_7_main.java
@@ -0,0 +1,6 @@
+public class Ass_7_main {
+    public static void main(String[] args) {
+        Ass_7 ss = new Ass_7();
+        ss.runFunction();
+    }
+}
