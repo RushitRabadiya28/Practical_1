b18ef91698afc80f524f9cd2de1666e28b5ab9f5
RushitRabadiya28 <110188571+RushitRabadiya28@users.noreply.github.com>
2022-07-31 23:16:14 +0530

    Update Ass_4_main.java

--- Ass_4_main.java
+++ Ass_4_main.java
@@ -1,13 +1,11 @@
 import java.util.Scanner;
-
 public class Ass_4_main {
     public static void main(String[] args) {
-
-        Scanner scs = new Scanner(System.in);
+        Scanner scs = new Scanner(System.in);  
 
         System.out.print("Enter the Size of Array -> ");
-        int aa = scs.nextInt();
-        Ass_4 as = new Ass_4(aa);
+        int aa = scs.nextInt();  // take input 
+        Ass_4 as = new Ass_4(aa);  // object make with pass argument 
         System.out.println("Created By:- Rushit Rabadiya \n" + "ID:- 21CE111");
     }
 }
