33d5562152ae6c59696ed875b3c7c829c8c6f52c
RushitRabadiya28 <110188571+RushitRabadiya28@users.noreply.github.com>
2022-07-31 23:12:04 +0530

    Update Ass_2_main.java

--- Ass_2_main.java
+++ Ass_2_main.java
@@ -3,10 +3,11 @@ import java.util.Scanner;
 public class Ass_2_main {
     public static void main(String[] args) {
         Scanner ssc=new Scanner(System.in);
-        Ass_2 as = new Ass_2();
+        Ass_2 as = new Ass_2(); // object make
         System.out.print("Enter String:");
         String A=ssc.nextLine();
-       as.startOZ(A);
-       System.out.println("Created By:- Rushit Rabadiya \n" + "ID:- 21CE111");
+       as.startOZ(A);//method call 
+        System.out.println("Created By:- Rushit Rabadiya \n" + "ID:- 21CE111");
     }
 }
+
