ac6c6f7e316ad4e5c0e604f9ee367eaf697945fd
RushitRabadiya28 <110188571+RushitRabadiya28@users.noreply.github.com>
2022-07-31 23:13:19 +0530

    Update Ass_3.java

--- Ass_3.java
+++ Ass_3.java
@@ -2,19 +2,20 @@ public class Ass_3 {
 
     public int a, b;
 
-    Ass_3() {
+    Ass_3() {  //default constructor 
     }
 
-    Ass_3(int x, int y) {
+    Ass_3(int x, int y) {  //parameter  constructor 
+
         a = x;
         b = y;
         {
-            a = a % 10;
+            a = a % 10;  //logic 
             b = b % 10;
         }
     }
 
-        void ans ()
+        void ans () // method 
         {
             if (a == b) {
                 System.out.println("TRUE");
