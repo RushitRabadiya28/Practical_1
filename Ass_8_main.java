05aea3d86013fe901a9cc88116b47b586b6e6808
RushitRabadiya28 <110188571+RushitRabadiya28@users.noreply.github.com>
2022-07-31 23:23:49 +0530

    Update Ass_8_main.java

--- Ass_8_main.java
+++ Ass_8_main.java
@@ -1,8 +1,8 @@
 public class Ass_8_main {
     public static void main(String[] args) {
 
-        Ass_8 ww = new Ass_8();
-        ww.exam();;
+        Ass_8 ww = new Ass_8(); // object  make 
+        ww.exam(); // method call 
         System.out.println("Created By:- Rushit Rabadiya \n" + "ID:- 21CE111");
 
     }
