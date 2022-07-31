52fb78f81f34c98e63facf07dda9f18ac709310f
RushitRabadiya28 <110188571+RushitRabadiya28@users.noreply.github.com>
2022-07-31 23:15:23 +0530

    Update Ass_4.java

--- Ass_4.java
+++ Ass_4.java
@@ -1,16 +1,15 @@
 import java.util.Scanner;
 
 public class Ass_4 {
-
      Scanner sc = new Scanner(System.in);
-     Ass_4(int a ) {
+     Ass_4(int a ) {  // parameter  constructor
          int x[] = new int[a];
          System.out.println("Enter your Array -> ");
          for (int i = 0; i < a; i++) {
-             x[i] = sc.nextInt();
+             x[i] = sc.nextInt();  // input Array 
          }
          int co=0;
-         for ( int i=0; i<a-2 ; i++) {
+         for ( int i=0; i<a-2 ; i++) {   // for logic 
              if (x[i] == 1 && x[i + 1] == 2 && x[i + 2] == 3) {
                  co++;
              }
@@ -27,14 +26,5 @@ public class Ass_4 {
              System.out.println("false (No) ");
          else
              System.out.println("true (Yes) ");
-
-
-
-
-     }
-
-
-     }
-
-
-
+}
+}
