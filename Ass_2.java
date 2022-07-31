cee6c6a67c660e45e7bb4eb06237b7881cd3294e
RushitRabadiya28 <110188571+RushitRabadiya28@users.noreply.github.com>
2022-07-31 23:11:23 +0530

    Update Ass_2.java

--- Ass_2.java
+++ Ass_2.java
@@ -1,14 +1,16 @@
+
 public class Ass_2 {
 
-    static void startOZ(String a)
-    {
-        if(a.charAt(0)=='O' || a.charAt(0)=='o')
-        {
-            System.out.print(a.charAt(0));
-        }
-        if(a.charAt(1)=='z' || a.charAt(1)=='Z')
-        {
-            System.out.print(a.charAt(1));
-        }
-    }
+static void startOZ(String a) // method 
+{
+// for check condition  
+if(a.charAt(0)=='O' || a.charAt(0)=='o')
+{
+System.out.print(a.charAt(0));
+}
+if(a.charAt(1)=='z' || a.charAt(1)=='Z')
+{
+System.out.print(a.charAt(1));
+}
+}
 }
