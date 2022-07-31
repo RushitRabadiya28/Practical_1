3d374d8f768ee31042d681aa6ef647daa8973f45
RushitRabadiya28 <110188571+RushitRabadiya28@users.noreply.github.com>
2022-07-31 23:17:58 +0530

    Update Ass_5_main.java

--- Ass_5_main.java
+++ Ass_5_main.java
@@ -1,13 +1,13 @@
 import java.util.*;
-
 public class Ass_5_main {
            public static void main(String[] args) {
             Scanner sc=new Scanner(System.in);
             System.out.println("Enter first string : ");
-            String s1=sc.next();
+            String s1=sc.next();  // input string 
             System.out.println("Enter second string : ");
-            String s2=sc.next();
-            Ass_5 d5=new Ass_5(s1,s2);
+            String s2=sc.next();  
+            Ass_5 d5=new Ass_5(s1,s2); // object make with pass argument
             sc.close();
         }
     }
+
