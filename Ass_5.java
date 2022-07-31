071f7a61db0c87b9044c54c448bf0cfff1df909f
RushitRabadiya28 <110188571+RushitRabadiya28@users.noreply.github.com>
2022-07-31 23:17:02 +0530

    Update Ass_5.java

--- Ass_5.java
+++ Ass_5.java
@@ -1,18 +1,15 @@
 import static java.lang.Math.min;
-
 public class Ass_5 {
-        Ass_5(String a, String b) {
-            int len = Math.min(a.length(), b.length());
+        Ass_5(String a, String b) {  // parameter  constructor
+            int len = Math.min(a.length(), b.length());  // math function for find minimum length 
             int count = 0;
-
             for (int i = 0; i < len - 1; i++) {
-                String aSub = a.substring(i, i + 2);
+                String aSub = a.substring(i, i + 2); // take substring  for check equals 
                 String bSub = b.substring(i, i + 2);
-                if (aSub.equals(bSub)) { 
+                if (aSub.equals(bSub)) {  
                     count++;
                 }
             }
             System.out.println("stringMatch(\"" + a + "\", \"" + b + "\") --> " + count);
         }
     }
-
