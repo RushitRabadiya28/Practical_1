fbf400213ae256b4b465ff41943bb2501691d0e3
RushitRabadiya28 <110188571+RushitRabadiya28@users.noreply.github.com>
2022-07-31 23:14:09 +0530

    Update Ass_3_main.java

--- Ass_3_main.java
+++ Ass_3_main.java
@@ -2,19 +2,16 @@ import java.util.Scanner;
 
 public class Ass_3_main {
     public static void main(String[] args) {
-        Ass_3 a1= new Ass_3();
+        Ass_3 a1= new Ass_3();  // object make 
 
         int a,b;
         System.out.print("Enter The Two Number =");
         Scanner sc = new Scanner(System.in);
-        a=sc.nextInt();
+        a=sc.nextInt(); // input int 
         b=sc.nextInt();
-       // PracticalList_3 c1 = new PracticalList_3(a ,b);
         System.out.printf("LASTDigit(%d,%d)",a,b);
         System.out.print(" -> ");
-        a1.ans();
+        a1.ans();  // method calling 
         System.out.println("Created By:- Rushit Rabadiya \n" + "ID:- 21CE111");
-
-
     }
 }
