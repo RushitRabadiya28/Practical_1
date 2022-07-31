47461f51972ff6f4ee29fc8ba298b2bbaf8cac96
RushitRabadiya28 <110188571+RushitRabadiya28@users.noreply.github.com>
2022-07-31 23:22:20 +0530

    Update Ass_7_main.java

--- Ass_7_main.java
+++ Ass_7_main.java
@@ -1,7 +1,7 @@
 public class Ass_7_main {
-    public static void main(String[] args) {
-        Ass_7 ss = new Ass_7();
-        ss.runFunction();
+      public static void main(String[] args) {
+        Ass_7 ss = new Ass_7(); // object make 
+        ss.runFunction();  // call method 
         System.out.println("Created By:- Rushit Rabadiya \n" + "ID:- 21CE111");
     }
 }
