e34aeb4de9b35fb16099507620645221610d5c11
RushitRabadiya28 <110188571+RushitRabadiya28@users.noreply.github.com>
2022-07-31 10:03:48 +0530

    Add files via upload

--- Ass_8_main.java
+++ Ass_8_main.java
@@ -1,3 +1,9 @@
 public class Ass_8_main {
+    public static void main(String[] args) {
 
+        Ass_8 ww = new Ass_8();
+        ww.exam();;
+        System.out.println("Created By:- Rushit Rabadiya \n" + "ID:- 21CE111");
+
+    }
 }
