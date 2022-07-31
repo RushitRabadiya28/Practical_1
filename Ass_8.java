ee0cef22a3448fb25e412dfe18008fb085a13f32
RushitRabadiya28 <110188571+RushitRabadiya28@users.noreply.github.com>
2022-07-31 23:23:02 +0530

    Update Ass_8.java

--- Ass_8.java
+++ Ass_8.java
@@ -1,9 +1,9 @@
 
-    import java.util.Scanner;
+       import java.util.Scanner;
 
     public class Ass_8 {
-        void exam() {
-            char student[][] = {
+        void exam() {  // method 
+            char student[][] = {  // default  input (ans of mcq)
                     {'A', 'B', 'A', 'C', 'C', 'D', 'E', 'E', 'A', 'D'},
                     {'D', 'B', 'A', 'B', 'C', 'A', 'E', 'E', 'A', 'D'},
                     {'E', 'D', 'D', 'A', 'C', 'B', 'E', 'E', 'A', 'D'},
@@ -13,21 +13,17 @@
                     {'B', 'B', 'A', 'C', 'C', 'D', 'E', 'E', 'A', 'D'},
                     {'E', 'B', 'E', 'C', 'C', 'D', 'E', 'E', 'A', 'D'}
             };
-            char ans[] = {'D', 'B', 'D', 'C', 'C', 'D', 'A', 'E', 'A', 'D'};
-            int a[] = new int[10];
-            
+            char ans[] = {'D', 'B', 'D', 'C', 'C', 'D', 'A', 'E', 'A', 'D'};  // ans of mcq 
+            int a[] = new int[10]; // make new array for store right answer  
             for (int i = 0; i < 8; i++) {
                 for (int j = 0; j < 10; j++) {
-                    if (student[i][j] == ans[j]) {
-                        a[i]++;
+                    if (student[i][j] == ans[j]) {  //  for checking propose 
+                        a[i]++;  // for Right Answer 
                     }
                 }
             }
-            for (int k = 0; k < 8; k++) {
+            for (int k = 0; k < 8; k++) {  // for print 
                 System.out.println("Student " + (k + 1) + " Score " + a[k] + " Marks out of 10 Marks");
             }
         }
     }
-
-
-
