f90cbdb6c7266ab4393880e2eeda67f39b4e4541
RushitRabadiya28 <110188571+RushitRabadiya28@users.noreply.github.com>
2022-07-31 23:26:20 +0530

    Update Ass_10.java

--- Ass_10.java
+++ Ass_10.java
@@ -1,14 +1,15 @@
 public class Ass_10 {
 
-    Ass_10 (String a) {
+    Ass_10 (String a) {  // parameter  constructor 
 
-        int l=a.length();
-        char b[] =a.toCharArray();
+        int l=a.length(); // for length of name 
+        char b[] =a.toCharArray(); // string convert into array
         for(int i=0;i<l;i++) {
-            b[i]+=3;
-            System.out.print(b[i]+" ");
+            b[i]+=3; // for +3 char  in all char
+            System.out.print(b[i]+" "); 
         }
     }
 }
+
 
 
