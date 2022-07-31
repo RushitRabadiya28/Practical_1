9f6117aef540f85a98c98b7f62f8509894ab3ced
RushitRabadiya28 <110188571+RushitRabadiya28@users.noreply.github.com>
2022-07-31 23:26:58 +0530

    Update Ass_10_main.java

--- Ass_10_main.java
+++ Ass_10_main.java
@@ -7,7 +7,7 @@ public class Ass_10_main {
         Scanner sc = new Scanner(System.in);
         String s;
         System.out.print("Enter The Name : ");
-        s = sc.next();
-        Ass_10 r= new Ass_10(s);
+        s = sc.next(); // input name 
+        Ass_10 r= new Ass_10(s); // object make with pass argument 
     }
 }
